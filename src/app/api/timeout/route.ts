f24f77c790b7119de1c402225c0383aa3bfa9981
Afonso Araújo Neto <afonso_mg3@hotmail.com>
2024-09-03 20:43:40 -0300

    fix(markdown-typescript): removing ts-ignore for readme file

--- src/app/api/timeout/route.ts
+++ src/app/api/timeout/route.ts
@@ -1,5 +1,4 @@
-//@ts-ignore
-import Readme from '@/../../README.md';
+import Readme from '../../../../README.md';
 
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
  
