ea7c81c31161906739aa85659b2541eab012ca56
Afonso Araújo Neto <afonso_mg3@hotmail.com>
2024-09-03 20:25:43 -0300

    feat(timeout-readme): using readme and ts-ignore (#16)

--- src/app/api/timeout/route.ts
+++ src/app/api/timeout/route.ts
@@ -1,5 +1,8 @@
+//@ts-ignore
+import Readme from '@/../../README.md';
+
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
  
 export function GET(request: Request) {
-  return new Response(`Timeout server route, add /[ms] to path`);
+  return new Response(`${Readme}`);
 }
